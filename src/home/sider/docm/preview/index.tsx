c7a77a8fb6e6bcd422583372472bdfa65c122fb3
姚文锐 <yaowr@landray.com.cn>
2019-12-10 09:20:46 +0800

    'react-pdf调试

--- src/home/sider/docm/preview/index.tsx
+++ src/home/sider/docm/preview/index.tsx
@@ -1,160 +1,56 @@
-import React from 'react'
-import { message, Button, InputNumber } from 'antd'
-import PDF from 'react-pdf-js'
+import React, { Component } from 'react'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 interface IProps {
   fileData: any
 }
-interface IState {
-  numPages: number
-  pageNumber: number
-  turnPageNumber: number
-  fileData: any
+
+interface IState {}
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true
 }
 
-class PDFViewer extends React.Component<IProps, IState> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      numPages: 0,
-      pageNumber: 1,
-      turnPageNumber: 0,
-      fileData: undefined
-    }
+export default class Sample extends Component<IProps, IState> {
+  state = {
+    numPages: null
   }
 
-  // componentDidMount() {
-  //   const PDFViewerWidth = document.getElementById('PDFViewer').offsetWidth
-  //   if (PDFViewerWidth < 650) {
-  //     throw Error('PDFViewer width must bigger than 650px')
-  //   }
-  // }
-
-  static getDerivedStateFromProps(nextProps, prevProps) {
-    const { fileData } = nextProps
-    if (fileData) {
-      console.log(fileData)
-      return { fileData }
-    }
-    return null
-  }
-
-  onDocumentComplete = numPages => {
+  onFileChange = event => {
     this.setState({
-      numPages,
-      pageNumber: 1
+      file: event.target.files[0]
     })
   }
 
-  onPageNumberInputChange = value => {
-    this.setState({
-      turnPageNumber: value
-    })
-  }
-
-  handleButtonOnChange = e => {
-    if (e.currentTarget.files.length === 0) return
-    const file = e.currentTarget.files[0]
-    const url = window.URL.createObjectURL(file)
-    this.setState({
-      fileData: {
-        key: url,
-        file: url
-      }
-    })
-  }
-
-  handleTurnPage = e => {
-    const { pageNumber, numPages, turnPageNumber } = this.state
-    if (!numPages) {
-      message.warning('请先选择PDF文件')
-      return
-    }
-    let newPageNumber = pageNumber
-    switch (e.target.id) {
-      case 'pageUp':
-        newPageNumber -= 1
-        if (newPageNumber <= 0) {
-          message.warning('已经是第一页')
-          return
-        }
-        break
-      case 'pageDown':
-        newPageNumber += 1
-        if (newPageNumber > numPages) {
-          message.warning('已经是最后一页')
-          return
-        }
-        break
-      case 'numberPage':
-        if (!turnPageNumber) {
-          message.warning('请先输入数字')
-          return
-        } else if (turnPageNumber <= 0 || turnPageNumber > numPages) {
-          message.warning('请输入在页面范围内的数字')
-          return
-        }
-        newPageNumber = turnPageNumber
-        break
-      default:
-        break
-    }
-    this.setState({
-      pageNumber: newPageNumber
-    })
-  }
-
-  createPDF = () => {
-    const { pageNumber, numPages, fileData } = this.state
-    if (!fileData) return
-    console.log(fileData)
-    return (
-      <div>
-        <div>
-          <PDF
-            key={'1'}
-            file={fileData}
-            onDocumentComplete={this.onDocumentComplete}
-            page={pageNumber}
-          />
-        </div>
-        <p style={{ float: 'right' }}>
-          第 {pageNumber} 页 共 {numPages} 页
-        </p>
-      </div>
-    )
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages })
   }
 
   render() {
+    const { numPages } = this.state
+    const { fileData } = this.props
     return (
-      <div id="PDFViewer">
-        {/* <input
-          id="id"
-          type="file"
-          style={{ width: '200px', height: '35px' }}
-          accept=".pdf"
-          onChange={this.handleButtonOnChange}
-        /> */}
-        <div style={{ float: 'right' }}>
-          <InputNumber
-            onChange={this.onPageNumberInputChange}
-            style={{ width: '150px' }}
-            placeholder="输入需要跳转的页"
-          />
-          <Button onClick={this.handleTurnPage} id="numberPage">
-            确认跳转
-          </Button>
-          <Button onClick={this.handleTurnPage} id="pageUp">
-            上一页
-          </Button>
-          <Button onClick={this.handleTurnPage} id="pageDown">
-            下一页
-          </Button>
+      <div>
+        <div>
+          <div>
+            <Document
+              file={fileData}
+              onLoadSuccess={this.onDocumentLoadSuccess}
+              options={options}
+            >
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  renderMode={'svg'}
+                />
+              ))}
+            </Document>
+          </div>
         </div>
-        {this.createPDF()}
       </div>
     )
   }
 }
-
-export default PDFViewer
